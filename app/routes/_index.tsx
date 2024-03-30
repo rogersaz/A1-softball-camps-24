8660c828775704f14cf074c1ec80cdecb246ab12
Mark <45799393+rogersaz@users.noreply.github.com>
2024-03-30 15:56:38 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -4,7 +4,7 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
+    <main className="relative min-h-screen bg-white sm: flex sm:items-center sm:justify-center">
       <div className="relative sm:pb-16 sm:pt-8">
         <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="relative shadow-xl sm:overflow-hidden sm:rounded-2xl">
@@ -17,7 +17,7 @@ export default function Index() {
               <div className="absolute inset-0 bg-[color:rgba(139,92,246,0.5)] mix-blend-multiply" />
             </div>
             <div className="lg:pb-18 relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pt-32">
-              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+              <h1 className="text-center text-4xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-violet-500 drop-shadow-md">
                   a1
                 </span>
@@ -26,11 +26,15 @@ export default function Index() {
                 </span>
                  <span className="block uppercase text-violet-500 drop-shadow-md">
                    OF - L/L
-                 </span>
+                </span>
               </h1>
-               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-               Sophomore Surprise, Ariz. Willow Canyon HS
+      
+              <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
+                I'll be playing for the 2024 Bradenton Slice, @a1softball
+                 from @LATechSB coming to the @FGCLsoftball
+                 this summer season for the Slice & Dice Machine! 
               </p>
+       
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
@@ -49,13 +53,14 @@ export default function Index() {
                     </Link>
                     <Link
                       to="/login"
-                      className="flex items-center justify-center rounded-md bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600  "
+                      className="flex items-center justify-center rounded-md bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600"
                     >
                       Log In
                     </Link>
                   </div>
                 )}
               </div>
+              
               <a href="https://remix.run">
                 <img
                   src="https://user-images.githubusercontent.com/1500684/158298926-e45dafff-3544-4b69-96d6-d3bcc33fc76a.svg"
@@ -66,54 +71,61 @@ export default function Index() {
             </div>
           </div>
         </div>
-
-        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-6 lg:px-8">
-  <div className="mt-6 flex flex-wrap justify-center gap-8">
-    {[
-      {
-        /* src: "https://user-images.githubusercontent.com/43764894/223575102-b784690b-19a6-4e7a-b72c-8ac42c736a71.png",
-        alt: "Netlify",
-        href: "https://netlify.com", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/8431042/158711352-746c52cf-433e-4823-987a-c9d6f4349ce7.svg",
-        alt: "Supabase",
-        href: "https://supabase.com", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
-        alt: "Tailwind",
-        href: "https://tailwindcss.com", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
-        alt: "Cypress",
-        href: "https://www.cypress.io", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
-        alt: "Testing Library",
-        href: "https://testing-library.com", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
-        alt: "Prettier",
-        href: "https://prettier.io", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
-        alt: "ESLint",
-        href: "https://eslint.org", */
-      },
-      {
-        /* src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
-        alt: "TypeScript",
-        href: "https://typescriptlang.org", */
-      }
-    ]}
-  </div>
-</div>
-
+          
+        <div className="mx-auto max-w-7xl py-2 px-4 sm:px-8 lg:px-10">
+          <div className="mt-6 flex flex-wrap justify-center gap-8">
+            {[
+              {
+                src: "/Slice-Softball-Team.png",
+                alt: "Slice Softball Team",
+                href: "http://www.fgclsoftball.com",
+              },
+              {
+                src: "/Slice-Softball-Team.png",
+                alt: "Supabase",
+                href: "https://supabase.com",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157764276-a516a239-e377-4a20-b44a-0ac7b65c8c14.svg",
+                alt: "Tailwind",
+                href: "https://tailwindcss.com",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157764454-48ac8c71-a2a9-4b5e-b19c-edef8b8953d6.svg",
+                alt: "Cypress",
+                href: "https://www.cypress.io",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157772662-92b0dd3a-453f-4d18-b8be-9fa6efde52cf.png",
+                alt: "Testing Library",
+                href: "https://testing-library.com",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157772934-ce0a943d-e9d0-40f8-97f3-f464c0811643.svg",
+                alt: "Prettier",
+                href: "https://prettier.io",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157772990-3968ff7c-b551-4c55-a25c-046a32709a8e.svg",
+                alt: "ESLint",
+                href: "https://eslint.org",
+              },
+              {
+                src: "https://user-images.githubusercontent.com/1500684/157773063-20a0ed64-b9f8-4e0b-9d1e-0b65a3d4a6db.svg",
+                alt: "TypeScript",
+                href: "https://typescriptlang.org",
+              },
+            ].map((img) => (
+              <a
+                key={img.href}
+                href={img.href}
+                className="flex h-16 w-32 justify-center p-1 grayscale transition hover:grayscale-0 focus:grayscale-0"
+              >
+                <img alt={img.alt} src={img.src} />
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     </main>
   );
