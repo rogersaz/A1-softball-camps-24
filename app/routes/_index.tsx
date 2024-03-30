2b457f11cd077540614c4a5e3be17c25f07628a2
Mark <45799393+rogersaz@users.noreply.github.com>
2024-03-29 19:01:04 -0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -28,10 +28,9 @@ export default function Index() {
                    OF - L/L
                  </span>
               </h1>
-             /* <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
-                Check the README.md file for instructions on how to get this
-                project deployed.
-              </p>  */
+               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
+               Sophomore Surprise, Ariz. Willow Canyon HS
+              </p>
               <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
                   <Link
