2789fb7af5d9f2a5791884587d087864bd2c6f06
Mark <45799393+rogersaz@users.noreply.github.com>
2024-07-07 09:02:12 -0700

    New favicon.ico

--- app/root.tsx
+++ app/root.tsx
@@ -17,7 +17,7 @@ import tailwindStylesheetUrl from "./styles/tailwind.css";
 import { getUser } from "./session.server";
 
 export const meta: MetaFunction = () => {
-  return [{ title: "New Remix App" }];
+  return [{ title: "A1 Softball Camps" }];
 };
 
 export const links: LinksFunction = () => {
