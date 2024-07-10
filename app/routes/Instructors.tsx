195995f60bfa37d17de3aee5c5a62dde5d192480
Mark <45799393+rogersaz@users.noreply.github.com>
2024-07-10 09:04:54 -0700

    new background

--- app/routes/Instructors.tsx
+++ app/routes/Instructors.tsx
@@ -30,7 +30,7 @@ export default function Index() {
               <div className="absolute inset-0">
                 <img
                   className="h-full w-full object-cover"
-                  src="/Softballs - Background.jpg"
+                  src="/softball-stack.jpg"
                   alt="Girls Softball Camp 2024 - Surprise AZ"
                 />
                 <div className="absolute inset-0 bg-[color:rgba(139,92,246,0.5)] mix-blend-multiply" />
