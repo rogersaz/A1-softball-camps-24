1d89fbb3387b61f5bc753be3a6a439aa74db9e8b
Mark <45799393+rogersaz@users.noreply.github.com>
2024-07-27 15:49:27 -0700

    Update Instructors.tsx

--- app/routes/Instructors.tsx
+++ app/routes/Instructors.tsx
@@ -149,7 +149,7 @@ export default function Index() {
                       </Link>
                     </>
                   )}
-                </div>
+                
 
                 
                   <Link
