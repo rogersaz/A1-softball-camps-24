5775a241be80aebc0ac3594184574db19d514a44
Mark <45799393+rogersaz@users.noreply.github.com>
2024-08-09 18:39:01 -0700

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,28 @@
-import styles from "./styles/globals.css"; 
+import type { LinksFunction } from "@remix-run/node";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import styles from "./styles/globals.css"; // Importing the global CSS
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles }, // Linking the global CSS
+];
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
 
 
 
